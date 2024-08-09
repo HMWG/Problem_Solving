1d35ba38bb139c400a0d5f3ad950fde247afc0f4
Minwoo Hwang <97011190+HMWG@users.noreply.github.com>
2024-08-09 15:44:07 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 16036 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,6 +11,7 @@ public class Main {
     static int w, h;
     static int[] dx = {-1, 0, 1, 0, 1, 1, -1, -1};
     static int[] dy = {0, -1, 0, 1, -1, 1, -1, 1};
+    static Queue<int[]> q = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -21,6 +24,7 @@ public class Main {
             }
             map = new int[w][h];
             visited = new boolean[w][h];
+            q = new LinkedList<>();
             int count = 0;
 
             for (int i = 0; i < h; i++) {
@@ -33,13 +37,33 @@ public class Main {
                 for (int j = 0; j < h; j++) {
                     if(map[i][j] == 1 && !visited[i][j]){
                         count++;
-                        dfs(i, j);
+                        bfs(i, j);
                     }
                 }
             }
             System.out.println(count);
         }
     }
+    public static void bfs(int x, int y){
+        if(visited[x][y]){
+            return;
+        }
+        visited[x][y] = true;
+        q.offer(new int[]{x, y});
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            for(int i = 0; i < 8; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if (check(nx, ny)){
+                    q.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+
+
     public static void dfs(int x, int y){
         if(visited[x][y]){
             return;
